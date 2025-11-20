83896f95d23697c3a349863a55f745e8f9fb261a
AlR0507 <160520283+AlR0507@users.noreply.github.com>
2025-11-20 13:51:43 -0800

    Override OnModelCreating

--- Data/NBADbContext.cs
+++ Data/NBADbContext.cs
@@ -7,7 +7,17 @@ namespace NBADATA.Data
     public class NBADbContext : IdentityDbContext<ApplicationUser>
     {
         public NBADbContext(DbContextOptions<NBADbContext> options) : base(options) { }
-
         public DbSet<Player> Players { get; set; } = null!;
+        public DbSet<FavoritePlayer> FavoritePlayers { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Un mismo usuario no puede tener el mismo jugador 2 veces en favoritos
+            builder.Entity<FavoritePlayer>()
+                .HasIndex(f => new { f.UserId, f.PlayerId })
+                .IsUnique();
+        }
     }
 }
